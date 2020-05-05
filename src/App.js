56740cf2874dc3819e606ed0b130c06797689327
Daedra <foglzerika@gmail.com>
2020-05-04 22:53:14 -0500

    feat | Creating section AboutUs

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bulma/css/bulma.css'
 import NavBar from "./components/NavBar/NavBar";
 import Hero from "./components/Hero/Hero";
 import Treatments from "./components/Treatments/Treatments";
+import AboutUs from "./components/AboutUs/AboutUs";
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <Hero/>
       <Treatments/>
+      <AboutUs/>
     </div>
   );
 }
