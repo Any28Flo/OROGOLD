9253c0cd509f710a64bf1215e812dc0ca630bf30
Daedra <foglzerika@gmail.com>
2020-05-05 21:14:04 -0500

    feat | Adding sección to Address component

--- src/components/Address/Address.js
+++ src/components/Address/Address.js
@@ -4,7 +4,7 @@ import Map from "./Map";
 const Address = () =>{
     return(
         <div>
-            <section className="hero  is-medium  section">
+            <section className="hero  is-medium">
                 <div className="hero-body">
 
                     <div className="container level">
@@ -14,10 +14,18 @@ const Address = () =>{
                             </div>
                         </div>
                         <div className="level-right">
-                            <div className="level-item">
-                                <h1>Horarios</h1>
-                                <p>Lunes a viernes de 10:00 a 19:30</p>
+                            <div className="level-item has-text-left">
+                                <div>
+                                  <h2 className="title is-uppercase">Horarios</h2>
+                                    <p className="">Lunes a viernes de 10:00 a 19:30</p>
+                                    <p>Sábados, domingos y fetivos de 10:00 a 18:30</p>
+                                    <p>Pueden concertarse tratamientos fuera del horario de apertura indicado con cita previa</p>
+                                    <h2 className="title is-uppercase">Dirección</h2>
+                                    <p>Av. Insurgentes Sur 105, Roma Sur, Cuahtémoc, 06600 Ciudad de México, CDMX, México</p>
+                                    <button className="button is-large ">Ver en mapa</button>
 
+
+                                </div>
                             </div>
                         </div>
 
